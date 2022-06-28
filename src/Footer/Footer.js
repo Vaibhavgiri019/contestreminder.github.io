6f92f7df0cf9d72b57898458d9ccab25880d67e0
Vaibhav Giri <girivaibhav852@gmail.com>
2022-06-28 19:46:30 +0530

    contestreminder

---
+++ src/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div>
+      <footer>
+        <div class="footer-content">
+            <h3>Vaibhav Giri</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo iste corrupti doloribus odio sed!</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-github"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy;2020 Vaibhav. designed by <span>Vaibhav Giri</span></p>
+        </div>
+    </footer>
+      </div>
+    )
+}
